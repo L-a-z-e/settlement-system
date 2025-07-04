592427af45cd36b76374ca164fcaa898d347497e
L-a-z-e <yysi8771@gmail.com>
2025-07-04 23:25:48 +0900

    first commit

---
+++ src/test/java/com/laze/settlementsystem/SettlementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laze.settlementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
